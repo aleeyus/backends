630d655569701b11405a1b8464e3ceedccd7f931
aleeyus <scamingsd@gmail.com>
2018-10-12 01:02:08 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+###########################################################################
+# here is the begining may God help us
+# please do make sure you left an explicit doumentation before commit
+# do not jokes with any scope 
+###########################################################################
+
+
+dynamic = 10
+print("This is the first project\n"*10)
+
+def classLess():
+    for x in range(1,11):
+        global dynamic
+        x = x * dynamic
+        dynamic = dynamic *10
+        print(x)
+
+classLess()
+print()
+print(dynamic)
